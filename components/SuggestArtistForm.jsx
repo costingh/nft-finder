a2a12157e62bd26d1e6b855881563da71f45d128
costin <chgeorghe0702@stud.acs.upb.ro>
2021-10-27 11:43:28 +0300

    fixed overlay

--- components/SuggestArtistForm.jsx
+++ components/SuggestArtistForm.jsx
@@ -7,6 +7,7 @@ export default function SuggestArtistForm({
   return (
     <div
       className={suggestArtistOpened ? styles.overlay : styles.overlayHidden}
+      style={{ background: "rgba(0,0,0,0.8)" }}
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
